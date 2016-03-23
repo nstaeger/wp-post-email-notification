7cd944713cc2637cbc9b7de905d5702928403d61
nstaeger <mail@nstaeger.de>
2016-03-23 18:31:15 +0100

    feat(framework): register OptionBroker

--- vendor/nstaeger/cms-plugin-framework/src/Creator/WordpressCreator.php
+++ vendor/nstaeger/cms-plugin-framework/src/Creator/WordpressCreator.php
@@ -6,11 +6,13 @@ use Nstaeger\CmsPluginFramework\Broker\AssetBroker;
 use Nstaeger\CmsPluginFramework\Broker\DatabaseBroker;
 use Nstaeger\CmsPluginFramework\Broker\EventBroker;
 use Nstaeger\CmsPluginFramework\Broker\MenuBroker;
+use Nstaeger\CmsPluginFramework\Broker\OptionBroker;
 use Nstaeger\CmsPluginFramework\Broker\RestBroker;
 use Nstaeger\CmsPluginFramework\Broker\Wordpress\WordpressAssetBroker;
 use Nstaeger\CmsPluginFramework\Broker\Wordpress\WordpressDatabaseBroker;
 use Nstaeger\CmsPluginFramework\Broker\Wordpress\WordpressEventBroker;
 use Nstaeger\CmsPluginFramework\Broker\Wordpress\WordpressMenuBroker;
+use Nstaeger\CmsPluginFramework\Broker\Wordpress\WordpressOptionsBroker;
 use Nstaeger\CmsPluginFramework\Broker\Wordpress\WordpressRestBroker;
 use Nstaeger\CmsPluginFramework\Plugin;
 
@@ -22,6 +24,7 @@ class WordpressCreator implements Creator
         $plugin->singleton(DatabaseBroker::class, WordpressDatabaseBroker::class);
         $plugin->singleton(EventBroker::class, WordpressEventBroker::class);
         $plugin->singleton(MenuBroker::class, WordpressMenuBroker::class);
+        $plugin->singleton(OptionBroker::class, WordpressOptionsBroker::class);
         $plugin->singleton(RestBroker::class, WordpressRestBroker::class);
     }
 }
