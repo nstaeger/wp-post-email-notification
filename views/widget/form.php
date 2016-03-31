49b88ae01da6e05e3bbad20fb4e14ca71d5842b7
nstaeger <mail@nstaeger.de>
2016-03-31 19:21:55 +0200

    refactor(widget): use 'echo' instead of '<?='

--- views/widget/form.php
+++ views/widget/form.php
@@ -1,8 +1,8 @@
 <p>
-    <label for="<?= $title_field_id ?>"><?php _e('Title:'); ?></label>
-    <input id="<?= $title_field_id ?>" name="<?= $title_field_name ?>" value="<?= $title_value ?>" type="text" class="widefat">
+    <label for="<?php echo $title_field_id; ?>"><?php _e('Title:'); ?></label>
+    <input id="<?php echo $title_field_id; ?>" name="<?php echo $title_field_name; ?>" value="<?php echo $title_value; ?>" type="text" class="widefat">
 </p>
 <p>
-    <label for="<?= $text_field_id ?>"><?php _e('text Text:'); ?></label>
-    <textarea id="<?= $text_field_id ?>" name="<?= $text_field_name ?>" class="widefat" cols="20" rows="8"><?= $text_value ?></textarea>
+    <label for="<?php echo $text_field_id; ?>"><?php _e('text Text:'); ?></label>
+    <textarea id="<?php echo $text_field_id; ?>" name="<?php echo $text_field_name; ?>" class="widefat" cols="20" rows="8"><?php echo $text_value; ?></textarea>
 </p>
--- views/widget/widget.php
+++ views/widget/widget.php
@@ -1,22 +1,22 @@
-<?= $before_widget ?>
+<?php echo $before_widget; ?>
 
 <?php if ($title) : ?>
-    <?= $before_title ?>
-    <?= $title ?>
-    <?= $after_title ?>
+    <?php echo $before_title; ?>
+    <?php echo $title; ?>
+    <?php echo $after_title; ?>
 <?php endif; ?>
 
 <?php if ($text) : ?>
-    <p><?= $text ?></p>
+    <p><?php echo $text; ?></p>
 <?php endif; ?>
 
 <form v-if="!success" v-on:submit.prevent="subscribe">
-    <input name="" type="email" placeholder="email address" v-model="subscriber.email">
-    <input type="submit" value="Submit">
+    <input name="email" type="email" placeholder="email address" v-model="subscriber.email">
+    <input type="submit" value="Submit" :disabled="currentlySubscribing">
 </form>
 
 <p class="success" v-if="success">
     Thanks for subscribing!
 </p>
 
-<?= $after_widget ?>
+<?php echo $after_widget; ?>
